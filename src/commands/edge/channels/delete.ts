66b63cba2d8a621e8be99295a600a8c1b75250de
Ross Stenersen <rossiam@gmail.com>
2022-09-09 10:41:24 -0500

    feat: remove managed config for default channel when deleting that channel

--- src/commands/edge/channels/delete.ts
+++ src/commands/edge/channels/delete.ts
@@ -1,3 +1,5 @@
+import { resetManagedConfigKey } from '@smartthings/cli-lib'
+
 import { chooseChannel } from '../../../lib/commands/channels-util'
 import { EdgeCommand } from '../../../lib/edge-command'
 
@@ -15,6 +17,7 @@ export default class ChannelsDeleteCommand extends EdgeCommand<typeof ChannelsDe
 	async run(): Promise<void> {
 		const id = await chooseChannel(this, 'Choose a channel to delete.', this.args.id)
 		await this.client.channels.delete(id)
+		await resetManagedConfigKey(this.cliConfig, 'defaultChannel', value => value === id)
 		this.log(`Channel ${id} deleted.`)
 	}
 }
